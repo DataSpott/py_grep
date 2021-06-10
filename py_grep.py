02c97972292147f625c14e627f0a5264725afb7d
dataspott <Ricc.Spott@gmx.de>
2021-06-10 15:54:52 +0200

    Implemented advanced sheet-recognition, deactivation of search-string & reverse-search

--- py_grep.py
+++ py_grep.py
@@ -4,6 +4,7 @@
 ## Module-import
 
 import pandas as pd
+from pandas.core.common import flatten
 import numpy as np
 import os
 import sys
@@ -19,6 +20,8 @@ parser.add_argument('-n', '--sheet', nargs = '+', help = "Number or name of the 
 parser.add_argument('-c', '--column', nargs = '+', help = "One or multiple columns to search in [default: all]", default = ['all'])
 parser.add_argument('-s', '--search', nargs = '+', help = "One or multiple terms to search for", default = False)
 parser.add_argument('-o', '--output', help = "Name of the output-directory", default = os.getcwd())
+parser.add_argument('-r', '--reverse', help = "Invert the search to search everything expect the search-terms", action = 'store_true', default = False)
+parser.add_argument('-d', '--deact_search_str', help = "Deactivates printing of searched-string in first row", action = 'store_true', default = False)
 parser.add_argument('--md', help = "Activate output of result-file in .md-format", action = 'store_true', default = False)
 parser.add_argument('--csv', help = "Activate output of result-file in .csv-format", action = 'store_true', default = False)
 parser.add_argument('--tsv', help = "Deactivate output of result-file in .tsv-format", action = 'store_false', default = True)
@@ -29,7 +32,9 @@ parser.add_argument('--notnull', help = "Search for notNaN [overwrites -s]", act
 arg = parser.parse_args()
 
 #define arguments as variables:
+deactivate_search_str = arg.deact_search_str
 input_file = arg.input
+invert_search =  arg.reverse
 sheet = arg.sheet
 search_column = arg.column
 search_term = arg.search
@@ -41,27 +46,44 @@ search_isnull = arg.isnull
 search_notnull = arg.notnull
 
 if tsv_output == False and csv_output == False and md_output == False:
-  sys.exit('>> Please activate at least one output-format. <<')
+	sys.exit('>> Please activate at least one output-format. <<')
 
 if search_term == False and search_isnull == False and search_notnull == False:
-  sys.exit('>> One of the following flags is required to start a search: [-s] [--isnull] [--notnull] <<')
+	sys.exit('>> One of the following flags is required to start a search: [-s] [--isnull] [--notnull] <<')
 
 if search_isnull == True and search_notnull == True:
-  sys.exit('>> Can´t search for null & notnull at the same time. <<')
+	sys.exit('>> Can´t search for null & notnull at the same time. <<')
 
 
 ################################################################################
 ## Function declarations
 
-def int_convert(element):
-  try:
-    return int(element)
-  except (ValueError, TypeError):
-    return element 
-
 def column_keyError(column, column_list):
-  if column != 'all' and column not in column_list:
-    sys.exit(">> KeyError: Column ['%s'] doesn´t exist in the dataframe. <<" %column)
+	if column != 'all' and column not in column_list:
+		sys.exit(">> KeyError: Column ['%s'] doesn´t exist in the dataframe. <<" %column)
+
+def int_convert(element):
+	try:
+		return int(element)
+	except (ValueError, TypeError):
+		return element 
+
+def range_parse(element, max_sheets):
+	parsed_sheet = []
+	if '-' in str(element):
+		element_subparts_list = str(element).split('-')
+		if element_subparts_list[0] != '':
+			element_first_subpart = int(element_subparts_list[0])
+		else:
+			element_first_subpart = 0
+		if element_subparts_list[1] != '':
+			element_second_subpart = int(element_subparts_list[1]) + 1
+		else:
+			element_second_subpart = max_sheets
+		parsed_sheet.extend(list(range(element_first_subpart,element_second_subpart)))
+	else:
+		parsed_sheet.append(element)
+	return parsed_sheet
 
 
 ################################################################################
@@ -73,17 +95,8 @@ os.makedirs(output_path, exist_ok = True)
 
 #check if output-flag was used:
 if output_path != os.getcwd():
-        
-    #change working directory to the path
-    os.chdir(output_path)
-
-
-################################################################################
-## Configure sheet
-
-sheet = [int_convert(element) for element in sheet]
-parsed_sheet = []
-[parsed_sheet.extend(list(range(int(element[0]),(int(element[2]) + 1)))) if '-' in str(element) else parsed_sheet.append(element) for element in sheet]
+	#change working directory to the path
+	os.chdir(output_path)
 
 
 ################################################################################
@@ -92,20 +105,27 @@ parsed_sheet = []
 excel_extensions_list = ['xls', 'xlsx', 'xlsm', 'xlsb', 'odf', 'ods', 'odt']
 
 if any(excel_extension in input_file for excel_extension in excel_extensions_list):
-  if len(parsed_sheet) == 0:
-    data = pd.concat(pd.read_excel(input_file, sheet_name = None), ignore_index = True)
-  elif len(parsed_sheet) == 1:
-    data = pd.read_excel(input_file, sheet_name = parsed_sheet[0])   
-  else:
-    data = pd.concat([pd.read_excel(input_file, sheet_name = number) for number in parsed_sheet], ignore_index = True)
+	max_sheet_nr = len(pd.ExcelFile(input_file).sheet_names)
+	sheet = [int_convert(element) for element in sheet]
+	parsed_sheet = list(flatten([range_parse(element,max_sheet_nr) for element in sheet]))
 
-elif ".csv" in input_file or ".tsv" in input_file:
-  data = pd.read_csv(input_file)
+	if len(parsed_sheet) == 0:
+		data = pd.concat(pd.read_excel(input_file, sheet_name = None), ignore_index = True)
+	elif len(parsed_sheet) == 1:
+		data = pd.read_excel(input_file, sheet_name = parsed_sheet[0])   
+	else:
+		data = pd.concat([pd.read_excel(input_file, sheet_name = number) for number in parsed_sheet], ignore_index = True)
+
+elif ".csv" in input_file:
+	data = pd.read_csv(input_file)
+
+elif ".tsv" in input_file:
+	data = pd.read_csv(input_file, sep='\t')
 
 else:
-  sys.exit('>> Unsupported input-file format. Use excel-, .csv- or .tsv-format instead <<')
+	sys.exit('>> Unsupported input-file format. Use excel-, .csv- or .tsv-format instead. <<')
 
-data = data.astype(str)
+data = data.astype(str).replace('nan', np.nan)
 
 [column_keyError(column, list(data.columns)) for column in search_column]
 
@@ -114,69 +134,78 @@ data = data.astype(str)
 ## Search
 
 if search_column[0] == 'all':
-  if search_isnull == True: 
-    data_searched = data[data.isna().any(axis=1)]
-    search_term = ['NaN']
-  elif search_notnull == True:
-    data_searched = data[data.notnull().any(axis=1)]
-    search_term = ['not NaN']
-  else:
-    search_str = '|'.join(search_term)
-    stacked_df = data.stack() # convert entire data frame into a series of values
-    data_searched = data.iloc[stacked_df[stacked_df.str.contains(search_str, case = False, na = False)].index.get_level_values(0).drop_duplicates()]
+	if search_isnull == True: 
+		data_searched = data[data.isna().any(axis=1)]
+		search_term = ['NaN']
+	elif search_notnull == True:
+		data_searched = data[data.notnull().any(axis=1)]
+		search_term = ['not NaN']
+	else:
+		search_str = '|'.join(search_term)
+		stacked_df = data.stack() # convert entire data frame into a series of values
+		if invert_search == False:
+			data_searched = data.iloc[stacked_df[stacked_df.str.contains(search_str, case = False, na = False)].index.get_level_values(0).drop_duplicates()]
+		else:
+			data_searched = data.iloc[stacked_df[~stacked_df.str.contains(search_str, case = False, na = False)].index.get_level_values(0).drop_duplicates()]
 
 else:
-  if search_isnull == True:
-    data_searched = data[data[search_column].isna().any(axis=1)]
-    search_term = ['NaN']
-  elif search_notnull == True:
-    data_searched = data[data[search_column].notnull().any(axis=1)]
-    search_term = ['not NaN']
-  else:
-    stacked_df = data[search_column].stack() # convert entire data frame into a series of values
-    data_searched = data.iloc[stacked_df[stacked_df.str.contains('|'.join(search_term), case = False, na = False)].index.get_level_values(0).drop_duplicates()]
+	if search_isnull == True:
+		data_searched = data[data[search_column].isna().any(axis=1)]
+		search_term = ['NaN']
+	elif search_notnull == True:
+		data_searched = data[data[search_column].notnull().any(axis=1)]
+		search_term = ['not NaN']
+	else:
+		stacked_df = data[search_column].stack() # convert entire data frame into a series of values
+		if invert_search == False:
+			data_searched = data.iloc[stacked_df[stacked_df.str.contains('|'.join(search_term), case = False, na = False)].index.get_level_values(0).drop_duplicates()]
+		else:
+			data_searched = data.iloc[stacked_df[~stacked_df.str.contains('|'.join(search_term), case = False, na = False)].index.get_level_values(0).drop_duplicates()]
 
 
 ################################################################################
 ## Output
 
-output_file_basename = "ROWGRAB_" + '_'.join(search_column) + "_searched" 
+output_file_basename = "pygrep_" + '_'.join(search_column) + "_searched" 
 
 if len(search_column) == 1:
-  search_column_str = "Searched column: ["
+	search_column_str = "Searched column: ["
 else:
-  search_column_str = "Searched columns: ["
+	search_column_str = "Searched columns: ["
 
 if len(search_term) == 1:
-  search_term_str = "] for following term: ["
+	search_term_str = "] for following term: ["
 else:
-  search_term_str = "] for following terms: ["
+	search_term_str = "] for following terms: ["
 
 searched_string = search_column_str + '; '.join(search_column) + search_term_str + '; '.join(search_term) + "]."
 
 if md_output == True:
-  output_file_md_name = output_file_basename + ".md"
-  result_md = data_searched.to_markdown()
-  result_file_md = open(output_file_md_name, "w")
-  result_file_md.write(searched_string)
-  result_file_md.write('\n')
-  result_file_md.write(result_md)
-  result_file_md.close()
+	output_file_md_name = output_file_basename + ".md"
+	result_md = data_searched.to_markdown()
+	result_file_md = open(output_file_md_name, "w")
+	if deactivate_search_str == False:
+		result_file_md.write(searched_string)
+		result_file_md.write('\n')
+	result_file_md.write(result_md)
+	result_file_md.close()
 
 if csv_output == True:
-  output_file_csv_name = output_file_basename + ".csv"
-  result_csv = data_searched.to_csv()
-  result_file_csv = open(output_file_csv_name, "w")
-  result_file_csv.write(searched_string)
-  result_file_csv.write('\n')
-  result_file_csv.write(result_csv)
-  result_file_csv.close()
+	output_file_csv_name = output_file_basename + ".csv"
+	result_csv = data_searched.to_csv()
+	result_file_csv = open(output_file_csv_name, "w")
+	if deactivate_search_str == False:
+		result_file_csv.write(searched_string)
+		result_file_csv.write('\n')
+	result_file_csv.write(result_csv)
+	result_file_csv.close()
 
 if tsv_output == True:
-  output_file_tsv_name = output_file_basename + ".tsv"
-  result_tsv = data_searched.to_csv(sep='\t')
-  result_file_tsv = open(output_file_tsv_name, "w")
-  result_file_tsv.write(searched_string)
-  result_file_tsv.write('\n')
-  result_file_tsv.write(result_tsv)
-  result_file_tsv.close()
+	output_file_tsv_name = output_file_basename + ".tsv"
+	result_tsv = data_searched.to_csv(sep='\t')
+	result_file_tsv = open(output_file_tsv_name, "w")
+	if deactivate_search_str == False:
+		result_file_tsv.write(searched_string)
+		result_file_tsv.write('\n')
+	result_file_tsv.write(result_tsv)
+	result_file_tsv.close()
